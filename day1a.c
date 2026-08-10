0f25e382cc42fcbd4a6caf75e992a0c1624eea74
C Evelyn Deborah <cevelyndeborah@gmail.com>
2026-08-10 16:23:46 +0000

    Question 1 of Day 1

---
+++ day1a.c
@@ -0,0 +1,17 @@
+//Q1: Write a program to input two numbers and display their sum.
+
+#include <stdio.h>
+
+int main()
+{
+    float a,b;
+    printf("Enter a and b: ");
+    scanf("%f%f",&a,&b);
+    
+    printf("The sum of %f and %f is %f",a,b,a+b);
+
+    return 0;
+}
+
+
+
