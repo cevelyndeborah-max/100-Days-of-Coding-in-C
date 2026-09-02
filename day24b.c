77f4593277dcc43469c9bdc51a7bde7fc3b80803
Evelyn <cevelyndeborah@gmail.com>
2026-09-02 18:08:48 +0530

    Question 48 of Day 24

---
+++ day24b.c
@@ -0,0 +1,15 @@
+//Q48: Write a program to print the following pattern:
+
+#include <stdio.h>
+int main()
+{
+    for(int a=1;a<=5;a++)
+    {
+        for(int b=1;b<=a;b++)
+        {
+            printf("%d",b);
+        }
+        printf("\n");
+    }
+    return 0;
+}
